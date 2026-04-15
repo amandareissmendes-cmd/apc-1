4e954125b44b7ab0078b9580d63a414cc1c58b32
Amanda <amandareissmendes@gmail.com>
2026-04-15 10:19:54 -0300

    conclui a pratica 2

---
+++ praticas/pratica02/imprima_caracteres.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main () {
+    /* entrada de dados*/
+    printf(" Imprimir o caractere A, uma quebra de linha \n e o texto Isso aqui eh uma string ");
+    return 0;
+}
---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main () {
+    /*entrada de dados*/
+    printf("Imprimir os números 10, -5 e 00004");
+
+    return 0;
+}
