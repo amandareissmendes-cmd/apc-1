73df5ee61838c86f4db3b8bd3e61ea35cd2c4bba
Amanda <amandareissmendes@gmail.com>
2026-05-13 08:58:59 -0300

    Corrige codigo do imprima_decimais

--- praticas/pratica02/imprima_decimais.c
+++ praticas/pratica02/imprima_decimais.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-int mais () {
+int main () {
     /*entrada de dados*/
     printf("imprimir o número 3.1415926 com todas as casas e também com apenas duas casas");
 
     return 0;
+}
