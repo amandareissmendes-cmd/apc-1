83191159fe9167cb787cae0e8be3bbadccde2b16
Amanda <amandareissmendes@gmail.com>
2026-05-13 08:47:31 -0300

    feat: conclui pratica06. Fecha #ID

---
+++ praticas/pratica03/declara_variaveis.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+    char tecla_pressionada;
+    int numero_inteiro;
+    
+    tecla_pressionada = 'A';
+    numero inteiro = 23;
+printf ("Qual sua idade? \m")
+
+
+    return 0;
+}
